7f4dc997794f571092652297175971276a03db6a
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 18:29:52 +0300

    Some corrections

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main()
-{
-unsigned int a ;
-cout<<"Square size: ";
-cin>>a;
-for(int i=0;i<a;i++) {
-for(int i=0;i<a;i++) cout<<"* ";
-cout<<endl;
-}
-return 0;
+ {
+  unsigned int a ;
+  cout<<"Square size: ";
+  cin>>a;
+  for(int i=0;i<a;i++) {
+    for(int i=0;i<a;i++) cout<<"* ";
+    cout<<endl;
+  }
+  return 0;
 }
 
