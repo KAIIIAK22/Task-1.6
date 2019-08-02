d4cf0b4f8a8ee6e23413c62a62409d1e8e5a0938
Pavel Poltaves <dzhura17@yandex.ru>
2019-08-02 17:36:51 +0300

    Create square
    Create square by it's size

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+unsigned int a ;
+cout<<"Square size: ";
+cin>>a;
+for(int i=0;i<a;i++) {
+for(int i=0;i<a;i++) cout<<"* ";
+cout<<endl;
+}
+return 0;
+}
+
